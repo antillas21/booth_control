76d95a532b1c9fb1d1189d789d6c36aa2d514421
Antonio Antillon <a.antillon@codigo-abierto.net>
2010-11-01 10:13:06 -0700

    Added seeding data for admin and map

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+admin = User.create(:name => "Admin", :email => 'antillas21@gmail.com', :password => '123456',
+:password_confirmation => '123456')
+User.first.update_attribute(:admin, true)
+
+Map.create!(:map_file => "path/to/map.swf", :zoom => "400%", :zoom_x => "300%", :zoom_y => "200%")
